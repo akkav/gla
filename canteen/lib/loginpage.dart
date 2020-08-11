c3bd0c3b0d69b888aa144b75e66a15f6341989ec
Akash <akka9936@gmail.com>
2020-08-11 15:54:41 +0530

    intial

---
+++ canteen/lib/loginpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class loginpage extends StatelessWidget {
+  const loginpage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("loginpage")),
+    );
+  }
+}
--- canteen/lib/main.dart
+++ canteen/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'loginpage.dart';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(home: Homepage()));
@@ -12,52 +14,43 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => loginpage(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromRGBO(3, 9, 23, 1),
-        body: SafeArea(
-          child: Row(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
             children: <Widget>[
               Container(
-                padding: EdgeInsets.all(30.0),
-                margin: EdgeInsets.only(top: 90.0),
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage('images/gla.png'),
-                      width: 70.0,
-                      height: 60.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(0.0),
-                      margin: EdgeInsets.only(top: 265.0),
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            'GLA                 ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                letterSpacing: 2.5,
-                                fontFamily: 'Chilanka'),
-                          ),
-                          Text(
-                            'CANTEEN',
-                            style: TextStyle(
-                                fontSize: 30.5,
-                                color: Colors.white,
-                                fontFamily: 'Chilanka',
-                                letterSpacing: 3.5),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
+                  width: 200.0,
+                  height: 200.0,
+                  margin: EdgeInsets.only(top: 250.0, bottom: 40.0),
+                  child: Image.asset("images/gla.png")),
+              Text(
+                "Welcome to canteen",
+                style: TextStyle(
+                    fontFamily: 'Chilanka',
+                    fontSize: 35.0,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+              ),
             ],
           ),
         ),
